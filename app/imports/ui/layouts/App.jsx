e865de450cdc3987eae3ca496a8516ab6ee6e789
James <nmfuntouchable@gmail.com>
2019-04-06 17:01:03 -1000

    Fixed add path to addFood

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -28,7 +28,7 @@ class App extends React.Component {
               <Route path="/signin" component={Signin}/>
               <Route path="/signup" component={Signup}/>
               <ProtectedRoute path="/list" component={ListStuff}/>
-              <ProtectedRoute path="/add" component={AddFood}/>
+              <ProtectedRoute path="/addFood" component={AddFood}/>
               <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
               <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
               <ProtectedRoute path="/signout" component={Signout}/>
