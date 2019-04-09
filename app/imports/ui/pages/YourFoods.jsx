a8a69ed15b19b8695a7b1dc7ff75cd7809a82fff
Christina Chen <46726195+christina-chen-cco2@users.noreply.github.com>
2019-04-09 10:10:03 -1000

    made navbar items white

--- app/imports/ui/pages/YourFoods.jsx
+++ app/imports/ui/pages/YourFoods.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Tab, Card, Image, Container, Header, Rating, Loader, Radio, Grid } from 'semantic-ui-react';
+import { Tab, Card, Image, Loader } from 'semantic-ui-react';
 import { Stuffs } from '/imports/api/stuff/stuff';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
@@ -123,48 +123,9 @@ class YourFoods extends React.Component {
         </Tab.Pane> },
     ];
 
-    const spacing = { paddingTop: '10px' };
     return (
         <div className='search-sidebar'>
-          <Grid>
-            <Grid.Row>
-              <Grid.Column>
-                <Header as='h2' content='Filters'/>
-                <Header as='h3' content='Search For'/>
-                <Container>
-                  <div className='two-options-toggle'>
-                    <div className='two-options-toggle-left'>
-                      Foods
-                    </div>
-                    <div className='two-options-toggle-right'>
-                      Reviews
-                    </div>
-                  </div>
-                  <Radio
-                      toggle label='Search Open Restaurants Only'
-                      style={spacing}
-                  />
-                  <Header as='h3' content='Rating'/>
-                  <Rating
-                      icon='heart'
-                      defaultRating={1}
-                      maxRating={5}
-                      size='massive'
-                  />
-                  <Header as='h3' content='Price'/>
-                  <Rating
-                      icon='star'
-                      defaultRating={1}
-                      maxRating={5}
-                      size='massive'
-                  />
-                </Container>
-              </Grid.Column>
-              <Grid.Column>
-                <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+          <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
         </div>
     );
   }
