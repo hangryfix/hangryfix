d22cb187de457827898151f5fd2ba4ae834d3647
Christina Chen <46726195+christina-chen-cco2@users.noreply.github.com>
2019-04-10 14:54:38 -1000

    Update YourFoods.jsx

--- app/imports/ui/pages/YourFoods.jsx
+++ app/imports/ui/pages/YourFoods.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Tab, Card, Image, Loader } from 'semantic-ui-react';
+import { Tab, Card, Image, Loader, Feed, Header } from 'semantic-ui-react';
+import SearchSidebar from '/../components/SearchSidebar';
 import { Stuffs } from '/imports/api/stuff/stuff';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
@@ -15,108 +16,101 @@ class YourFoods extends React.Component {
 
     const panes = [
       { menuItem: 'Favorites', render: () => <Tab.Pane fluid attached={false}>
-          <Card.Group centered>
-            <Card>
-              <Image src='' />
+          <Card.Group>
+            <Card centered>
               <Card.Content>
-                <Card.Header>FOOD NAME</Card.Header>
-                <Card.Meta>stuff</Card.Meta>
-                <Card.Description>stuff</Card.Description>
+                <Image floated='left' style={{ width: '40%' }}
+                       src='https://i.ibb.co/r2Cp0TK/froyo.jpg' />
+                <Card.Header>Froyo</Card.Header>
+                <Card.Meta>from Tutti Frutti</Card.Meta>
+                <Card.Meta>700 Keeaumoku St #102, Honolulu HI 96814</Card.Meta>
+                <Card.Description>
+                  Hours: M-Su 10am-10pm
+                </Card.Description>
               </Card.Content>
-              <Card.Content>
-                stuff
-              </Card.Content>
-            </Card>
-            <Card>
-              <Image src='' />
-              <Card.Content>
-                <Card.Header>FOOD NAME</Card.Header>
-                <Card.Meta>stuff</Card.Meta>
-                <Card.Description>stuff</Card.Description>
-              </Card.Content>
-              <Card.Content>
-                stuff
+              <Card.Content extra>
+                <Feed>
+                  <Header as='h4'>Review from kturner44</Header>
+                  <p>Love the froyo here! Lots of flavors and toppings to choose from!</p>
+                </Feed>
               </Card.Content>
             </Card>
-            <Card>
-              <Image src='' />
+            <Card centered>
               <Card.Content>
-                <Card.Header>FOOD NAME</Card.Header>
-                <Card.Meta>stuff</Card.Meta>
-                <Card.Description>stuff</Card.Description>
+                <Image floated='left' style={{ width: '40%' }}
+                       src='https://i.ibb.co/8cJFSLv/kalua-nachos.jpg' />
+                <Card.Header>Kalua Pig Nachos</Card.Header>
+                <Card.Meta>from Bay View Bar & Bistro</Card.Meta>
+                <Card.Meta>45-285 Kaneohe Bay Dr, Kaneohe HI 96744</Card.Meta>
+                <Card.Description>
+                  Hours: M-Su 11am-7pm
+                </Card.Description>
               </Card.Content>
-              <Card.Content>
-                stuff
-              </Card.Content>
-            </Card>
-            <Card>
-              <Image src='' />
-              <Card.Content>
-                <Card.Header>FOOD NAME</Card.Header>
-                <Card.Meta>stuff</Card.Meta>
-                <Card.Description>stuff</Card.Description>
-              </Card.Content>
-              <Card.Content>
-                stuff
+              <Card.Content extra>
+                <Feed>
+                  <Header as='h4'>Review from kturner44</Header>
+                  <p>These nachos are huge for only $13. Super ono and plenty to share if you
+                    get as an appetizer.  Or if you dont want to share, makes a good meal.</p>
+                </Feed>
               </Card.Content>
             </Card>
-            <Card>
-              <Image src='' />
+            <Card centered>
               <Card.Content>
-                <Card.Header>FOOD NAME</Card.Header>
-                <Card.Meta>stuff</Card.Meta>
-                <Card.Description>stuff</Card.Description>
+                <Image floated='left' style={{ width: '40%' }}
+                       src='https://i.ibb.co/n05BzTg/okonomiyaki.jpg' />
+                <Card.Header>Okonomiyaki</Card.Header>
+                <Card.Meta>from Waikiki Yokocho</Card.Meta>
+                <Card.Meta>2250 Kalakaua Ave, Honolulu HI 96815</Card.Meta>
+                <Card.Description>
+                  Hours: M-Su 11am-12am
+                </Card.Description>
               </Card.Content>
-              <Card.Content>
-                stuff
-              </Card.Content>
-            </Card>
-            <Card>
-              <Image src='' />
-              <Card.Content>
-                <Card.Header>FOOD NAME</Card.Header>
-                <Card.Meta>stuff</Card.Meta>
-                <Card.Description>stuff</Card.Description>
-              </Card.Content>
-              <Card.Content>
-                stuff
-              </Card.Content>
-            </Card>
-            <Card>
-              <Image src='' />
-              <Card.Content>
-                <Card.Header>FOOD NAME</Card.Header>
-                <Card.Meta>stuff</Card.Meta>
-                <Card.Description>stuff</Card.Description>
-              </Card.Content>
-              <Card.Content>
-                stuff
+              <Card.Content extra>
+                <Feed>
+                  <Header as='h4'>Review from kturner44</Header>
+                  <p>A little pricey, but some of the best Okonomiyaki you can find on the island.</p>
+                </Feed>
               </Card.Content>
             </Card>
           </Card.Group>
         </Tab.Pane> },
       { menuItem: 'Your Additions', render: () => <Tab.Pane attached={false}>
-          <Card.Group centered>
-            <Card>
-              <Image src='' />
+          <Card.Group>
+            <Card centered>
               <Card.Content>
-                <Card.Header>FOOD NAME</Card.Header>
-                <Card.Meta>stuff</Card.Meta>
-                <Card.Description>stuff</Card.Description>
+                <Image floated='left' style={{ width: '40%' }}
+                       src='https://i.ibb.co/r2Cp0TK/froyo.jpg' />
+                <Card.Header>Froyo</Card.Header>
+                <Card.Meta>from Tutti Frutti</Card.Meta>
+                <Card.Meta>700 Keeaumoku St #102, Honolulu HI 96814</Card.Meta>
+                <Card.Description>
+                  Hours: M-Su 10am-10pm
+                </Card.Description>
               </Card.Content>
-              <Card.Content>
-                stuff
+              <Card.Content extra>
+                <Feed>
+                  <Header as='h4'>Review from kturner44</Header>
+                  <p>Love the froyo here! Lots of flavors and toppings to choose from!</p>
+                </Feed>
               </Card.Content>
             </Card>
-            <Card>
-              <Image src='' />
+            <Card centered>
               <Card.Content>
-                <Card.Header>FOOD NAME</Card.Header>
-                <Card.Meta>stuff</Card.Meta>
-                <Card.Description>stuff</Card.Description>
+                <Image floated='left' style={{ width: '40%' }}
+                       src='https://i.ibb.co/8cJFSLv/kalua-nachos.jpg' />
+                <Card.Header>Kalua Pig Nachos</Card.Header>
+                <Card.Meta>from Bay View Bar & Bistro</Card.Meta>
+                <Card.Meta>45-285 Kaneohe Bay Dr, Kaneohe HI 96744</Card.Meta>
+                <Card.Description>
+                  Hours: M-Su 11am-7pm
+                </Card.Description>
               </Card.Content>
-              <Card.Content>
-                stuff
+              <Card.Content extra>
+                <Feed>
+                  <Header as='h4'>Review from kturner44</Header>
+                  <p>These nachos are huge for only $13. Super ono and plenty to share if you
+                    get as an appetizer.  Or if you dont want to share, makes a good meal.</p>
+                </Feed>
               </Card.Content>
             </Card>
           </Card.Group>
