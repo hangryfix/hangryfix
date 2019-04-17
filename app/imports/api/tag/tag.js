e0a7e456a537ad818378eb7ec25b496870b4085f
James <nmfuntouchable@gmail.com>
2019-04-17 10:18:01 -1000

    updated schema for an array

--- app/imports/api/tag/tag.js
+++ app/imports/api/tag/tag.js
@@ -3,18 +3,16 @@ import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
 
 /** Create a Meteor collection. */
-const Restaurants = new Mongo.Collection('Restaurants');
+const Tags = new Mongo.Collection('Tags');
 
 /** Create a schema to constrain the structure of documents associated with this collection. */
-const RestaurantSchema = new SimpleSchema({
+const TagSchema = new SimpleSchema({
   name: String,
-  address: String,
-  hours: Array,
-  'hours.$': Number,
+  type: String,
 }, { tracker: Tracker });
 
 /** Attach this schema to the collection. */
-Restaurants.attachSchema(RestaurantSchema);
+Foods.attachSchema(TagSchema);
 
 /** Make the collection and schema available to other code. */
-export { Restaurants, RestaurantSchema };
+export { Tags, TagSchema };
