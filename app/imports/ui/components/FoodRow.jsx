f4ea1aad780d4fb3ed4b020cb4f243bf6ccbc00e
Katrina H. Turner <khj@hawaii.edu>
2019-04-22 11:10:42 -1000

    Added FoodRow to Admin Food List

--- app/imports/ui/components/FoodRow.jsx
+++ app/imports/ui/components/FoodRow.jsx
@@ -6,8 +6,27 @@ import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { _ } from 'underscore';
 import Review from './Review';
+import { Foods } from '/imports/api/food/food';
 
-class Food extends React.Component {
+
+class FoodRow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick = () => Food.remove(this.props.contact._id, this.deleteCallback)
+
+  /** Notify the user of the results of the delete. */
+  deleteCallback(error) {
+    if (error) {
+      Bert.alert({ type: 'danger', message: `Delete failed: ${error.message}` });
+    } else {
+      Bert.alert({ type: 'success', message: 'Food Deleted' });
+    }
+  }
+
+
   render() {
 
     let averageRating = '';
@@ -48,7 +67,64 @@ class Food extends React.Component {
                   'No ratings yet.'
               )
               }
-
+              {this.props.reviews.length > 0 ? (
+                  <Modal size='small' trigger={<Button fluid>Show {this.props.reviews.length} ratings and reviews</Button>}>
+                    <Modal.Header>
+                      <Card fluid>
+                        <Card.Content>
+                          <Image floated='left' style={{ width: '30%' }} src={this.props.food.image} />
+                          <Card.Header style={{ fontSize: '30px' }}>
+                            {this.props.food.name}
+                          </Card.Header>
+                          <Card.Meta style={{ paddingBottom: '30px' }}>
+                            {this.props.reviews.length > 0 ? (
+                                <Rating size="huge" icon="heart" defaultRating={averageRating} maxRating={5} disabled/>
+                            ) : (
+                                'No ratings yet.'
+                            )
+                            }
+                          </Card.Meta>
+                          <Card.Meta style={{ fontSize: '16px', padding: '2px' }}>
+                            <Icon name="map marker alternate" style={{ marginRight: '5px' }}/>
+                            {this.props.food.restaurant}
+                          </Card.Meta>
+                          <Card.Meta style={{ fontSize: '16px', padding: '2px' }}>
+                            <Icon name="clock" style={{ marginRight: '5px' }} />
+                            {this.props.food.hours}
+                          </Card.Meta>
+                          <Card.Meta style={{ fontSize: '16px', padding: '2px' }}>
+                            <Icon name="dollar sign" />
+                            <Rating size="large"
+                                    icon="star"
+                                    defaultRating={this.props.food.price}
+                                    maxRating={5}
+                                    disabled/>
+                          </Card.Meta>
+                          <Card.Description>
+                            {this.props.food.description}
+                          </Card.Description>
+                        </Card.Content>
+                        <Card.Content>
+                          {this.props.food.tags.map((tag, index) => <
+                              Label tag
+                                    style={{ backgroundColor: '#338D33', color: 'white' }}
+                                    key={index}>
+                            {tag.name}
+                          </Label>)}
+                        </Card.Content>
+                      </Card>
+                    </Modal.Header>
+                    <Modal.Content scrolling>
+                      {this.props.reviews.map((review, index) => <Review
+                          key={index}
+                          review={review}
+                      />)}
+                    </Modal.Content>
+                  </Modal>
+              ) : (
+                  <Card.Header style={{ fontSize: '18px' }}>No reviews yet.</Card.Header>
+              )
+              }
             </Table.Cell>
 
           {/*Col 4: tags*/}
@@ -60,79 +136,26 @@ class Food extends React.Component {
             </Label>)}
           </Table.Cell>
 
-
-          <Card.Content>
-            {this.props.reviews.length > 0 ? (
-                <Modal size='small' trigger={<Button fluid>Show {this.props.reviews.length} ratings and reviews</Button>}>
-                  <Modal.Header>
-                    <Card fluid>
-                      <Card.Content>
-                        <Image floated='left' style={{ width: '30%' }} src={this.props.food.image} />
-                        <Card.Header style={{ fontSize: '30px' }}>
-                          {this.props.food.name}
-                        </Card.Header>
-                        <Card.Meta style={{ paddingBottom: '30px' }}>
-                          {this.props.reviews.length > 0 ? (
-                              <Rating size="huge" icon="heart" defaultRating={averageRating} maxRating={5} disabled/>
-                          ) : (
-                              'No ratings yet.'
-                          )
-                          }
-                        </Card.Meta>
-                        <Card.Meta style={{ fontSize: '16px', padding: '2px' }}>
-                          <Icon name="map marker alternate" style={{ marginRight: '5px' }}/>
-                          {this.props.food.restaurant}
-                        </Card.Meta>
-                        <Card.Meta style={{ fontSize: '16px', padding: '2px' }}>
-                          <Icon name="clock" style={{ marginRight: '5px' }} />
-                          {this.props.food.hours}
-                        </Card.Meta>
-                        <Card.Meta style={{ fontSize: '16px', padding: '2px' }}>
-                          <Icon name="dollar sign" />
-                          <Rating size="large"
-                                  icon="star"
-                                  defaultRating={this.props.food.price}
-                                  maxRating={5}
-                                  disabled/>
-                        </Card.Meta>
-                        <Card.Description>
-                          {this.props.food.description}
-                        </Card.Description>
-                      </Card.Content>
-                      <Card.Content>
-                        {this.props.food.tags.map((tag, index) => <
-                          Label tag
-                                style={{ backgroundColor: '#338D33', color: 'white' }}
-                                key={index}>
-                          {tag.name}
-                        </Label>)}
-                      </Card.Content>
-                    </Card>
-                  </Modal.Header>
-                  <Modal.Content scrolling>
-                    {this.props.reviews.map((review, index) => <Review
-                        key={index}
-                        review={review}
-                    />)}
-                  </Modal.Content>
-                </Modal>
-            ) : (
-                <Card.Header style={{ fontSize: '18px' }}>No reviews yet.</Card.Header>
-            )
-            }
-          </Card.Content>
+          {/*Col 5: options*/}
+          <Table.Cell>
+            <Button.Group>
+              <Button>Edit</Button>
+              <Button content='Delete' onClick={this.onClick}/>
+            </Button.Group>
+          </Table.Cell>
         </Table.Row>
     );
   }
 }
-Food.propTypes = {
+
+FoodRow.propTypes = {
   food: PropTypes.object.isRequired,
   currentUser: PropTypes.string,
   reviews: PropTypes.array.isRequired,
 };
 
-const FoodContainer = withTracker(() => ({
+const FoodRows = withTracker(() => ({
   currentUser: Meteor.user() ? Meteor.user().username : '',
-}))(Food);
+}))(FoodRow);
 
-export default withRouter(FoodContainer);
+export default withRouter(FoodRows);
